f83460a0c3239d3c1fec7514b794f1da813a2f92
Rauf Alkan <rauf.alkan@tedu.edu.tr>
2026-03-28 00:34:56 +0300

    ıooınoı

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  serverExternalPackages: ['fluent-ffmpeg'],
   experimental: {
     serverComponentsExternalPackages: ['fluent-ffmpeg', '@ffmpeg-installer/ffmpeg'],
   },
 }
+
 module.exports = nextConfig
